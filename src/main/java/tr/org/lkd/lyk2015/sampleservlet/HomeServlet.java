6c22386502b5c813d0fc15a4002dc4e9c6f4f2ea
Fatih ERDEM <93.fatiherdem@gmail.com>
2015-08-08 17:43:52 +0300

    Hello Message Added

--- src/main/java/tr/org/lkd/lyk2015/sampleservlet/HomeServlet.java
+++ src/main/java/tr/org/lkd/lyk2015/sampleservlet/HomeServlet.java
@@ -1,4 +1,25 @@
 package tr.org.lkd.lyk2015.sampleservlet;
 
-public class HomeServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/hello")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+
+        System.out.println("in hello Servlet");
+
+        req.setAttribute("message", "this is my message");
+
+        req.getRequestDispatcher("WEB-INF/hello.jsp").forward(req, res);
+
+    }
 }
